b7b0f4e1db16a378c62b8a0ef19bceeeecf2bd30
Diabloalex666 <61434532+Diabloalex666@users.noreply.github.com>
2026-06-18 15:49:27 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Настройки FinPulse / Calculators
 window.FINPULSE_CONFIG = {
-  siteUrl: "https://diabloalex666.github.io/Calculators/",
+  siteUrl: "https://https://finraz.ru/",
   boostyUrl: "https://boosty.to/alexeyvasilev",
   yandexMetrikaId: "109961609",
   rsyaBlockId: "",
