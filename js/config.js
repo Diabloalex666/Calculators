4d3bc21578062c326cf1464142e865fcf465fd45
Diabloalex666 <61434532+Diabloalex666@users.noreply.github.com>
2026-06-18 16:01:57 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,9 @@
-// Настройки FinPulse / Calculators
+// Настройки сайта FinPulse — https://finraz.ru/
 window.FINPULSE_CONFIG = {
   siteUrl: "https://finraz.ru/",
   boostyUrl: "https://boosty.to/alexeyvasilev/donate",
   yandexMetrikaId: "109961609",
+  yandexWebmasterVerification: "",
+  yandexAutoplacementPageId: "19466291",
   rsyaBlockId: "",
 };
